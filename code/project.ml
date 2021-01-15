cc4ede468ed137478e02a6a3dedaead3db2348ff
SauzeauYannis <sauzeauyannis@gmail.com>
2021-01-15 18:59:31 +0100

    explicit args name

--- code/project.ml
+++ code/project.ml
@@ -1,25 +1,25 @@
 (** Authors 	
 	CHEVAIS Benjamin 
 	      && 
-	SAUZEAU YANNIS
+	SAUZEAU Yannis
 **)
 
 (* Uncomment the load of your ocaml version *)
 
 (* OCaml version 4.02.3 *)
-(* #directory "../libraries/4.02.3/";; *)
+(* #directory "libraries/4.02.3/";; *)
 
 (* OCaml version 4.05.0 *)
- #directory "libraries/4.05.0/";; 
+#directory "libraries/4.05.0/";; 
 
 (* OCaml version 4.08.1 *)
-(* #directory "../libraries/4.08.1/";;*)
+#directory "libraries/4.08.1/";;
 
 (* OCaml version 4.10.0 *)
-(* #directory "../libraries/4.10.0/";; *)
+(* #directory "libraries/4.10.0/";; *)
 
 (* OCaml version 4.11.1 *)
-(* #directory "../libraries/4.11.1/";; *)
+(* #directory "libraries/4.11.1/";; *)
 
 
 #load "btree.cmo";;
@@ -33,29 +33,29 @@ open Random;;
 
 (* Question 1 *)
 
-let rec list_rnd_create(n, borne : int * int) : int list =
+let rec list_rnd_create(node_number, limit : int * int) : int list =
   Random.self_init();
-  let rd : int = Random.int borne in
-  if(n = 0)
+  let rd : int = Random.int limit in
+  if(node_number = 0)
   then []
-  else rd::list_rnd_create(n-1, borne)
+  else rd::list_rnd_create(node_number-1, limit)
 ;;
 
 list_rnd_create(10, 100);;
 
-let rec bst_rnd_create(n : int) : 'a t_btree =
-  bst_lbuild(list_rnd_create(n, 50))
+let rec bst_rnd_create(node_number, limit : int * int) : 'a t_btree =
+  bst_lbuild(list_rnd_create(node_number, limit))
 ;;
 
-let b1 = bst_rnd_create(10);;
+let b1 = bst_rnd_create(10, 100);;
 show_int_btree(b1);;
 
 (* Question 2 *)
 
 (* Calcule le déséquilibre d'un arbre *)
-let weight_balance(t : int t_btree) : int;;
+let weight_balance(tree : int t_btree) : int;;
 
-let average_weight_balance(node_number, tree_number : int, int) : float;;
+let average_weight_balance(node_number, tree_number, limit : int * int * int) : float;;
 
 (* Question 3 *)
 
