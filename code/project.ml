b1a21cbb7c496d68781137d78447b9524d1ad907
SauzeauYannis <sauzeauyannis@gmail.com>
2021-01-18 11:54:57 +0100

    add comments and quest 4 func prototypes

--- code/project.ml
+++ code/project.ml
@@ -34,17 +34,23 @@ open List;;
 
 (* Question 1 *)
 
-let rec list_rnd_create(node_number, limit : int * int) : int list =
+(** Créé une liste d'entiers aléatoires **)
+(* size : taille de la liste *)
+(* limit : nombre aléatoire maximal *)
+let rec list_rnd_create(size, limit : int * int) : int list =
   Random.self_init();
   let rd : int = Random.int limit in
-  if(node_number = 0)
+  if(size = 0)
   then []
-  else rd::list_rnd_create(node_number-1, limit)
+  else rd::list_rnd_create(size-1, limit)
 ;;
 
 list_rnd_create(10, 100);;
 
-let rec bst_rnd_create(node_number, limit : int * int) : 'a t_btree =
+(** Créé un arbre binaire de recherche d'entiers aléatoires **)
+(* node_number : nombre de noeuds *)
+(* limit : nombre aléatoire maximal *)
+let rec bst_rnd_create(node_number, limit : int * int) : 'a bst =
   bst_lbuild(list_rnd_create(node_number, limit))
 ;;
 
@@ -53,14 +59,18 @@ show_int_btree(b1);;
 
 (* Question 2 *)
 
+(** Retourne le maximum entre 2 nombres **)
+(* n1 : nombre 1 *)
+(* n2 : nombre 2 *)
 let max(n1, n2 : int * int) : int =
   if n1 > n2
   then n1
   else n2
 ;;
 
-(* Calcule le hauteur d'un arbre *)
-let rec height(tree : 'a t_btree) : int =
+(** Calcule le hauteur d'un arbre binaire de recherche **)
+(* tree : arbre binaire de recherche *)
+let rec height(tree : 'a bst) : int =
   if isEmpty(tree) || (isEmpty(rson(tree)) && isEmpty(lson(tree)))
   then 0
   else 1 + max(height(rson(tree)), height(lson(tree)))
@@ -68,8 +78,9 @@ let rec height(tree : 'a t_btree) : int =
 
 height(b1);;
 
-(* Calcule le déséquilibre d'un arbre *)
-let rec weight_balance(tree : int t_btree) : int =
+(** Calcule le déséquilibre d'un arbre binaire de recherche **)
+(* tree : arbre binaire de recherche *)
+let rec weight_balance(tree : int bst) : int =
   if isEmpty(tree)
   then 0
   else weight_balance(lson(tree)) + weight_balance(rson(tree)) +
@@ -77,6 +88,10 @@ let rec weight_balance(tree : int t_btree) : int =
 ;;
 
 weight_balance(b1);;
+
+(*
+
+Ces deux fonctions peuvent être regroupé en 1 seule (cf sum_weight_balance)
 
 let rec create_weight_balance_list(node_number, tree_number, limit : int * int * int) : int list =
   if(tree_number = 0)
@@ -91,15 +106,91 @@ let rec sum_list(list : int list) : int =
   else hd(list) + sum_list(tl(list))
 ;;
 
+*)
+
+(** Calcule la somme de déséquilibre de plusieurs arbres binaires de recherche **)
+(* node_number : nombre de noeuds par arbre *)
+(* tree_number : nombre d'arbres binaire de recherche à générer *)
+(* limit : nombre aléatoire maximal *)
+let rec sum_weight_balance(node_number, tree_number, limit : int * int * int) : float =
+  if (tree_number = 0)
+  then 0.
+  else let weight_balance : float = float_of_int(weight_balance(bst_rnd_create(node_number, limit))) in
+       weight_balance +. sum_weight_balance(node_number, tree_number-1, limit)
+;;
+
+(* 
+
+Ca raccourci aussi cette fonction
+
 let average_weight_balance(node_number, tree_number, limit : int * int * int) : float =
   let weight_balance_list = create_weight_balance_list(node_number, tree_number, limit) in
   let sum_weight_balance_list = sum_list(weight_balance_list) in
   float_of_int(sum_weight_balance_list) /. float_of_int(tree_number)
 ;;
+ *)
+
+(** Calcule la moyenne de déséquilibre de plusieurs arbres binaires de recherche **)
+(* node_number : nombre de noeuds par arbre *)
+(* tree_number : nombre d'arbres binaire de recherche à générer *)
+(* limit : nombre aléatoire maximal *)
+let average_weight_balance(node_number, tree_number, limit : int * int * int) : float =
+  sum_weight_balance(node_number, tree_number, limit) /. float_of_int(tree_number)
+;;
 
 average_weight_balance(10, 20, 50);;
 
 (* Question 3 *)
+
+(** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs aléatoire **)
+(* size : taille de la liste *)
+(* limit : nombre aléatoire maximal *)
+let rec list_rnd_create_rnd_sublist(size, limit : int * int) : int list =
+;;
+
+(** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs fixe **)
+(* size : taille de la liste *)
+(* limit : nombre aléatoire maximal *)
+let rec list_rnd_create_regular_sublist(size, limit : int * int) : int list =
+;;
+
+(** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs croissantes **)
+(* size : taille de la liste *)
+(* limit : nombre aléatoire maximal *)
+let rec list_rnd_create_inc_sublist(size, limit : int * int) : int list =
+;;
+
+(** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs décroissantes **)
+(* size : taille de la liste *)
+(* limit : nombre aléatoire maximal *)
+let rec list_rnd_create_dec_sublist(size, limit : int * int) : int list =
+;;
+
+(** Créé un arbre binaire de recherche d'entiers aléatoires avec des sous-suites ordonnées **)
+(* node_number : nombre de noeuds *)
+(* limit : nombre aléatoire maximal *)
+(* func : fonction de création de sous-suites ordonnées *)
+let rec bst_sublist_rnd_create(node_number, limit, func : int * int * (int * int -> int list)) : 'a bst =
+    bst_lbuild(func(node_number, limit))
+;;
+
+(** Calcule la somme de déséquilibre de plusieurs arbres binaires de recherche avec des sous-suites ordonnées **)
+(* node_number : nombre de noeuds par arbre *)
+(* tree_number : nombre d'arbres binaire de recherche à générer *)
+(* limit : nombre aléatoire maximal *)
+(* func : fonction de création de sous-suites ordonnées *)
+let rec sum_weight_balance_bis(node_number, tree_number, limit, func : int * int * int * (int * int -> int list)) : float =
+  0.
+;;
+
+(** Calcule la moyenne de déséquilibre de plusieurs arbres binaires de recherche avec des sous-suites ordonnées **)
+(* node_number : nombre de noeuds par arbre *)
+(* tree_number : nombre d'arbres binaire de recherche à générer *)
+(* limit : nombre aléatoire maximal *)
+(* func : fonction de création de sous-suites ordonnées *)
+let average_weight_balance_bis(node_number, tree_number, limit, func : int * int * int * (int * int -> int list)) : float =
+  0.
+;;
 
 (* Question 4 *)
 
