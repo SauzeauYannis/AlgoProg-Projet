edb439724e267685f221dc97c8a2cd4499e535b4
SauzeauYannis <sauzeauyannis@gmail.com>
2021-01-23 19:10:46 +0100

    list_rnd_create_dec_sl

--- code/project.ml
+++ code/project.ml
@@ -158,7 +158,23 @@ list_rnd_create_inc_sl(5, 100, function x -> x * 2);;
 (* limit : nombre aléatoire maximal *)
 (* order : fonctions d'ordre sur les sous-suites *)
 let rec list_rnd_create_dec_sl(nb_sl, limit, order : int * int * (int -> int)) : int list =
+  Random.self_init();
+  let rec list_create(nb_sl, m, limit, order, tmp : int * int * int * (int -> int) * int) : int list =
+    if nb_sl = 0
+    then []
+    else
+      let n : int = order(tmp)
+      in if m = 1
+         then n::list_create(nb_sl-1, nb_sl-1, limit, order, n)
+         else
+           if m <> nb_sl
+           then n::list_create(nb_sl, m-1, limit, order, n)
+           else let n : int = Random.int limit
+                in n::list_create(nb_sl, m-1, limit, order, n)
+  in list_create(nb_sl, nb_sl, limit, order, 0)
 ;;
+
+list_rnd_create_dec_sl(5, 100, function x -> x * 2);;
 
 (** Créé un arbre binaire de recherche d'entiers aléatoires avec des sous-suites ordonnées **)
 (* node_number : nombre de noeuds *)
