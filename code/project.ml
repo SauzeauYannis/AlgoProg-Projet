d32768fb66dc88cd393d94ba6c84d21b4b374122
SauzeauYannis <sauzeauyannis@gmail.com>
2021-01-23 18:07:24 +0100

    change list_rnd_create_regular_sl

--- code/project.ml
+++ code/project.ml
@@ -78,23 +78,16 @@ let rec weight_balance(tree : int bst) : int =
 
 weight_balance(b1);;
 
-(** Calcule la somme de déséquilibre de plusieurs arbres binaires de recherche **)
-(* node_number : nombre de noeuds par arbre *)
-(* tree_number : nombre d'arbres binaire de recherche à générer *)
-(* limit : nombre aléatoire maximal *)
-let rec sum_weight_balance(node_number, tree_number, limit : int * int * int) : float =
-  if (tree_number = 0)
-  then 0.
-  else let weight_balance : float = float_of_int(weight_balance(bst_rnd_create(node_number, limit))) in
-       weight_balance +. sum_weight_balance(node_number, tree_number-1, limit)
-;;
-
 (** Calcule la moyenne de déséquilibre de plusieurs arbres binaires de recherche **)
 (* node_number : nombre de noeuds par arbre *)
 (* tree_number : nombre d'arbres binaire de recherche à générer *)
 (* limit : nombre aléatoire maximal *)
 let average_weight_balance(node_number, tree_number, limit : int * int * int) : float =
-  sum_weight_balance(node_number, tree_number, limit) /. float_of_int(tree_number)
+  let rec sum(node_number, tree_number, limit : int * int * int) : int =
+    if (tree_number = 0)
+    then 0
+    else weight_balance(bst_rnd_create(node_number, limit)) + sum(node_number, tree_number-1, limit)
+  in float_of_int(sum(node_number, tree_number, limit)) /. float_of_int(tree_number)
 ;;
 
 average_weight_balance(10, 20, 100);;
@@ -109,37 +102,26 @@ let rec list_rnd_create_rnd_sl(size, limit, order : int * int * (int -> int)) : 
 ;;
 
 (** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs fixe **)
-(* size : taille de la liste *)
-(* sl_size : taille des sous-suites *)
+(* nb_sl : nombre sous-liste *)
+(* limit : nombre aléatoire maximal *)
 (* order : fonctions d'ordre sur les sous-suites *)
-let list_rnd_create_regular_sl(size, limit, order : int * int * (int -> int)) : int list =
-  let rec found_multiple(n, m : int * int) : int =
-    if m = 1
-    then -1
+let list_rnd_create_regular_sl(nb_sl, limit, order : int * int * (int -> int)) : int list =
+  Random.self_init();
+  let rec list_create(nb_sl, size, limit, order, tmp : int * int * int * (int -> int) * int) : int list =
+    if size = 0
+    then []
     else
-      if (n mod m) = 0
-      then m
-      else found_multiple(n, m-1)
-  in let mult : int = found_multiple(size, size-1)
-     in if mult = -1
-        then invalid_arg "size can't be a prime number"
-        else (
-          Random.self_init();
-          let rec list_create(size, limit, order, tmp : int * int * (int -> int) * int) : int list =
-            if size = 0
-            then []
-            else
-              if (size mod mult) == 0
-              then (
-                let rd : int = Random.int limit in
-                rd::list_create(size-1, limit, order, rd))
-              else (
-                let n : int = order(tmp) in
-                n::list_create(size-1, limit, order, n))
-          in list_create(size, limit, order, 0))
+      if (size mod 2) == 0
+      then (
+        let rd : int = Random.int limit in
+        rd::list_create(nb_sl, size-1, limit, order, rd))
+      else (
+        let n : int = order(tmp) in
+        n::list_create(nb_sl, size-1, limit, order, n))
+  in list_create(nb_sl, nb_sl * 2, limit, order, 0)
 ;;
 
-list_rnd_create_regular_sl(9, 100, function x -> x * 2);;
+list_rnd_create_regular_sl(5, 100, function x -> x * 2);;
   
 (** Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs croissantes **)
 (* size : taille de la liste *)
@@ -169,19 +151,6 @@ show_int_btree(b2);;
 let b3 = bst_sl_rnd_create(9, 100, (function x -> x + 2), list_rnd_create_regular_sl);;
 show_int_btree(b3);;
 
-(** Calcule la somme de déséquilibre de plusieurs arbres binaires de recherche avec des sous-suites ordonnées **)
-(* node_number : nombre de noeuds par arbre *)
-(* tree_number : nombre d'arbres binaire de recherche à générer *)
-(* limit : nombre aléatoire maximal *)
-(* order : fonctions d'ordre sur les sous-suites *)
-(* func : fonction de création de sous-suites ordonnées *)
-let rec sum_weight_balance_bis(node_number, tree_number, limit, order, func : int * int * int * (int -> int) * (int * int * (int -> int) -> int list)) : float =
-  if (tree_number = 0)
-  then 0.
-  else let weight_balance : float = float_of_int(weight_balance(bst_sl_rnd_create(node_number, limit, order, func))) in
-       weight_balance +. sum_weight_balance_bis(node_number, tree_number-1, limit, order, func)
-;;
-
 (** Calcule la moyenne de déséquilibre de plusieurs arbres binaires de recherche avec des sous-suites ordonnées **)
 (* node_number : nombre de noeuds par arbre *)
 (* tree_number : nombre d'arbres binaire de recherche à générer *)
@@ -189,7 +158,11 @@ let rec sum_weight_balance_bis(node_number, tree_number, limit, order, func : in
 (* order : fonctions d'ordre sur les sous-suites *)
 (* func : fonction de création de sous-suites ordonnées *)
 let average_weight_balance_bis(node_number, tree_number, limit, order, func : int * int * int * (int -> int) * (int * int * (int -> int) -> int list)) : float =
-  sum_weight_balance_bis(node_number, tree_number, limit, order, func) /. float_of_int(tree_number)
+  let rec sum(node_number, tree_number, limit, order, func : int * int * int * (int -> int) * (int * int * (int -> int) -> int list)) : int =
+    if (tree_number = 0)
+    then 0
+    else weight_balance(bst_sl_rnd_create(node_number, limit, order, func)) + sum(node_number, tree_number-1, limit, order, func)
+  in float_of_int(sum(node_number, tree_number, limit, order, func)) /. float_of_int(tree_number)
 ;;
 
 average_weight_balance_bis(9, 20, 100, (function x -> x * x), list_rnd_create_regular_sl);;
