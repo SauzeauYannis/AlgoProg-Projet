1286d87929069cc0397a84b6891cac9228d7830d
SauzeauYannis <sauzeauyannis@gmail.com>
2021-02-05 14:20:59 +0100

    comment ABR.ml

--- code/ABR.ml
+++ code/ABR.ml
@@ -4,7 +4,7 @@
 	SAUZEAU Yannis
 **)
 
-(* Uncomment the load of your ocaml version *)
+(* Décommenter la commande '#directory ...' selon votre version d'OCaml *)
 
 (* OCaml version 4.02.3 *)
 (* #directory "libraries/4.02.3/";; *)
@@ -21,13 +21,11 @@
 (* OCaml version 4.11.1 *)
 (* #directory "libraries/4.11.1/";; *)
 
-#load "btree.cmo";;
-#load "bst.cmo";;
+#load "btree.cmo";; 
+#load "bst.cmo";;   
 
-open Btree;;
-open Bst;;
-open Random;;
-open List;;
+open Btree;;  (* Module pour les arbres binaires *)
+open Bst;;    (* Module pour les arbres binaires de recherche *)
 
 (*** 1 Arbres Binaires de recherche ***)
 
@@ -40,10 +38,11 @@ let bst_rnd_create(node_number, limit : int * int) : 'a bst =
     let rd : int = Random.int limit in
     if size = 0
     then []
-    else rd::list_rnd_create(size-1, limit)
+    else rd::list_rnd_create(size - 1, limit)
   in bst_lbuild(list_rnd_create(node_number, limit))
 ;;
 
+(* Arbre à 10 noeuds de valeurs entre 0 et 99 *)
 let b1 = bst_rnd_create(10, 100);;
 show_int_btree(b1);;
 
@@ -60,6 +59,7 @@ let rec weight_balance(tree : 'a bst) : int =
      else height(lson(tree)) - height(rson(tree))
 ;;
 
+(* Déséquilibre de l'arbre b1 *)
 weight_balance(b1);;
 
 (* Calcule la moyenne de déséquilibre de plusieurs arbres binaires de recherche *)
@@ -71,30 +71,32 @@ let average_weight_balance(node_number, tree_number, limit : int * int * int) : 
   in (float_of_int (sum(node_number, tree_number, limit))) /. (float_of_int tree_number)
 ;;
 
+(* Moyenne de déséquilibre de 100 arbres à 10 noeuds de valeurs entre 0 et 99 *)
 average_weight_balance(10, 100, 100);;
 
 (* Question 3 *)
 
-(* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs aléatoire *)
+(* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs aléatoires entre 1 et 5 *)
 let rec list_rnd_create_rnd_sl(nb_sl, limit, order : int * int * (int -> int)) : int list =
   if nb_sl = 0
   then []
   else (
     Random.self_init();
-    let size_sl = Random.int nb_sl+1 in
+    let size_sl = (Random.int 5) + 1 in
     let rec create_list(size_sl, tmp : int * int) : int list =
       if size_sl = 0
       then []
       else let rd = order(tmp) in
-           rd::create_list(size_sl-1, rd);
+           rd::create_list(size_sl - 1, rd)
     in let rdval = Random.int limit in
-       create_list(size_sl, rdval) @ list_rnd_create_rnd_sl(nb_sl-1, limit, order)
+       create_list(size_sl, rdval) @ list_rnd_create_rnd_sl(nb_sl - 1, limit, order)
   )
 ;;
 
+(* Liste avec 5 sous-suites de longueurs aléatoires entre 1 et 5 ordonnée par incrémentation *)
 list_rnd_create_rnd_sl(5, 100, function x -> x + 1);;
 
-(* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs fixe *)
+(* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs 3 *)
 let list_rnd_create_regular_sl(nb_sl, limit, order : int * int * (int -> int)) : int list =
   Random.self_init();
   let rec list_create(nb_sl, size, limit, order, tmp : int * int * int * (int -> int) * int) : int list =
@@ -103,10 +105,11 @@ let list_rnd_create_regular_sl(nb_sl, limit, order : int * int * (int -> int)) :
     else let n = if (size mod 3) == 0
                  then Random.int limit
                  else order(tmp)
-         in n::list_create(nb_sl, size-1, limit, order, n)
+         in n::list_create(nb_sl, size - 1, limit, order, n)
   in list_create(nb_sl, nb_sl * 3, limit, order, 0)
 ;;
 
+(* Liste avec 5 sous-suites de longueurs 3 ordonnée par incrémentation *)
 list_rnd_create_regular_sl(5, 100, function x -> x + 1);;
   
 (* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs croissantes *)
@@ -118,12 +121,13 @@ let rec list_rnd_create_inc_sl(nb_sl, limit, order : int * int * (int -> int)) :
     else
       if m = size
       then let n : int = Random.int limit
-           in n::list_create(nb_sl, size+1, 1, limit, order, n)
+           in n::list_create(nb_sl, size + 1, 1, limit, order, n)
       else let n : int = order(tmp)
-           in n::list_create(nb_sl, size, m+1, limit, order, n)
+           in n::list_create(nb_sl, size, m + 1, limit, order, n)
   in list_create(nb_sl, 0, 0, limit, order, 0)
 ;;
 
+(* Liste avec 5 sous-suites de longueurs croissantes ordonnée par incrémentation *)
 list_rnd_create_inc_sl(5, 100, function x -> x + 1);;
 
 (* Créé une liste d'entiers aléatoires avec des sous-suites ordonnées de longueurs décroissantes *)
@@ -136,16 +140,17 @@ let rec list_rnd_create_dec_sl(nb_sl, limit, order : int * int * (int -> int)) :
       if m = nb_sl
       then let n : int = Random.int limit
            in if m = 1
-              then n::list_create(nb_sl-1, m-1, limit, order, n)
-              else n::list_create(nb_sl, m-1, limit, order, n)
+              then n::list_create(nb_sl - 1, m - 1, limit, order, n)
+              else n::list_create(nb_sl, m - 1, limit, order, n)
       else
         let n : int = order(tmp)
         in if m = 1
-           then n::list_create(nb_sl-1, nb_sl-1, limit, order, n)
-           else n::list_create(nb_sl, m-1, limit, order, n)
+           then n::list_create(nb_sl - 1, nb_sl - 1, limit, order, n)
+           else n::list_create(nb_sl, m - 1, limit, order, n)
   in list_create(nb_sl, nb_sl, limit, order, 0)
 ;;
 
+(* Liste avec 5 sous-suites de longueurs décroissantes ordonnée par incrémentation *)
 list_rnd_create_dec_sl(5, 100, function x -> x + 1);;
 
 (* Créé un arbre binaire de recherche d'entiers aléatoires avec des sous-suites ordonnées *)
@@ -153,6 +158,7 @@ let bst_sl_rnd_create(nb_sl, limit, order, func : int * int * (int -> int) * (in
     bst_lbuild(func(nb_sl, limit, order))
 ;;
 
+(* Arbre binaire créé à partir de 5 sous-suites de longueurs 3 ordonnée par incrémentation *)
 let b2 = bst_sl_rnd_create(5, 100, (function x -> x + 1), list_rnd_create_regular_sl);;
 show_int_btree(b2);;
 
@@ -165,22 +171,22 @@ let average_weight_balance_sl(nb_sl, tree_number, limit, order, func : int * int
   in (float_of_int (sum(nb_sl, tree_number, limit, order, func))) /. (float_of_int tree_number)
 ;;
 
+(* Moyenne du déséquilibre pour 100 arbres créé à partir de 5 sous-suites de longueurs aléatoires entre 1 et 5 *)
 average_weight_balance_sl(5, 100, 100, (function x -> x * x), list_rnd_create_rnd_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x * 2), list_rnd_create_rnd_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x + 2), list_rnd_create_rnd_sl);;
 
+(* Moyenne du déséquilibre pour 100 arbres créé à partir de 5 sous-suites de longueurs 3 *)
 average_weight_balance_sl(5, 100, 100, (function x -> x * x), list_rnd_create_regular_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x * 2), list_rnd_create_regular_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x + 2), list_rnd_create_regular_sl);;
 
+(* Moyenne du déséquilibre pour 100 arbres créé à partir de 5 sous-suites de longueurs croissantes *)
 average_weight_balance_sl(5, 100, 100, (function x -> x * x), list_rnd_create_inc_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x * 2), list_rnd_create_inc_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x + 2), list_rnd_create_inc_sl);;
 
+(* Moyenne du déséquilibre pour 100 arbres créé à partir de 5 sous-suites de longueurs décroissantes *)
 average_weight_balance_sl(5, 100, 100, (function x -> x * x), list_rnd_create_dec_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x * 2), list_rnd_create_dec_sl);;
 average_weight_balance_sl(5, 100, 100, (function x -> x + 2), list_rnd_create_dec_sl);;
-
-(* Question 4 *)
-
-(* Faire en PDF ? *)
